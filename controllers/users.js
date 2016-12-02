620f084ea364460d587647eaff96923ef7337d08
Nick Oki <nick.oki@live.com>
2016-12-01 21:35:35 -0500

    Refactor sign up

--- controllers/users.js
+++ controllers/users.js
@@ -27,7 +27,24 @@ exports.signup = (req, res) => {
     // Save new User
     newUser.save( err => {
       if (err) res.send(err)
-      else res.json({ success: true, message: 'Welcome! User created successfully.' })
+      else {
+        User.findOne({ email: req.body.email }, function(err, user) {
+          if (err) throw err
+          if (!user) res.send({ success: false, message: 'Authentication failed: User not found.' })
+          else {
+            // Verify password
+            user.checkPassword(req.body.password, function(err, isMatch) {
+              if (isMatch && !err) {
+                // Assign token
+                var token = jwt.sign({ userId: user._id, username: user.username }, db.secret, { algorithm: 'HS256' })
+                res.json({ success: true, token: `JWT ${token}`, username: user.username })
+              }
+              else res.send({ success: false, message: 'Authentication failed: Incorrect password.' })
+            })
+          }
+        })
+        // res.json({ success: true, message: 'Welcome! User created successfully.' })
+      }
     })
   }
 }
